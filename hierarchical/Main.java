9be5e78ac555b92218cd1e9a62b5047aa7826269
anjanaajikumar <anjanaajikumar@gmail.com>
2021-05-26 21:30:31 +0530

    change

---
+++ hierarchical/Main.java
@@ -0,0 +1,15 @@
+package hierarchical;
+
+public class Main {
+    public static void main(String args[]){
+    Teachers t1=new Teachers();
+    t1.name="Tr.Anu";
+    t1.phone=23456;
+    t1.disp();
+    
+    Students s1=new Students();
+    s1.name="Adhi";
+    s1.phone=6789;
+    s1.disp();
+    }
+}
