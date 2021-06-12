80fd1bb632caace1946b5b58d76f750f382ea654
anjanaajikumar <anjanaajikumar@gmail.com>
2021-06-12 17:10:09 +0530

    map

--- collection/map.java
+++ collection/map.java
@@ -1,6 +1,30 @@
 package collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class map {
-     
     
-}
+    public static void main(String args[]){
+      Map<Integer,String> m=new HashMap<>();
+      m.put(501,"Adarsh");
+      m.put(502,"Adhi");
+      m.put(503,"Adarsh");
+      
+      System.out.println(m.containsKey(504)); 
+      System.out.println(m.size());
+      System.out.println(m.get(502));
+      
+      System.out.println(m.entrySet()); //returns the entryset
+      
+      //printing using for loop
+      for (Map.Entry e: m.entrySet()){
+        System.out.println(e.getKey() + " " + e.getValue()); 
+      }
+      
+      
+      }
+      
+    }
+    
+
